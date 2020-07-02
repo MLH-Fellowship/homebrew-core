bcf1a05bc3424e58542d4060d49e997257437f38
William Ma <whoiswillma@gmail.com>
2020-07-02 11:49:50 -0400

    n: add license

--- Formula/n.rb
+++ Formula/n.rb
@@ -3,6 +3,7 @@ class N < Formula
   homepage "https://github.com/tj/n"
   url "https://github.com/tj/n/archive/v6.5.1.tar.gz"
   sha256 "5833f15893b9951a9ed59487e87b6c181d96b83a525846255872c4f92f0d25dd"
+  license "MIT"
   head "https://github.com/tj/n.git"
 
   bottle do
