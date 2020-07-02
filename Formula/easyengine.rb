8ccf82a7f5548359c00c97b80bee60a3ccbfe378
William Ma <whoiswillma@gmail.com>
2020-07-02 11:48:06 -0400

    easyengine: add license

--- Formula/easyengine.rb
+++ Formula/easyengine.rb
@@ -3,6 +3,7 @@ class Easyengine < Formula
   homepage "https://easyengine.io/"
   url "https://github.com/EasyEngine/easyengine/releases/download/v4.0.17/easyengine.phar"
   sha256 "78db155bc9786002c00070007d6f61d3403caf98e57d648d4bcf4d883ed5c141"
+  license "MIT"
 
   bottle :unneeded
 
